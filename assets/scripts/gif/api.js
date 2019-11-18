8a4782534869bbf56e022b1697d07c27841651b5
asooge <asooge@gmail.com>
2019-11-18 12:08:42 -0500

    update api url

--- assets/scripts/gif/api.js
+++ assets/scripts/gif/api.js
@@ -2,7 +2,7 @@
 
 const getGifs = function (search) {
   return $.ajax({
-    url: `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=UW6UfEEO71CGk2AZgexrHeGYiTRAy1GK&limit=6`,
+    url: `//api.giphy.com/v1/gifs/search?q=${search}&api_key=UW6UfEEO71CGk2AZgexrHeGYiTRAy1GK&limit=6`,
     headers: {
       api_key: 'UW6UfEEO71CGk2AZgexrHeGYiTRAy1GK',
       q: search,
