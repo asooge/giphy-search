1a707b265b37687e2b05ef9aa2cf7e945195c2ca
asooge <asooge@gmail.com>
2019-11-18 13:10:27 -0500

    update ajax request to allow cors

--- assets/scripts/gif/api.js
+++ assets/scripts/gif/api.js
@@ -3,12 +3,13 @@
 const getGifs = function (search) {
   return $.ajax({
     url: `//api.giphy.com/v1/gifs/search?q=${search}&api_key=UW6UfEEO71CGk2AZgexrHeGYiTRAy1GK&limit=6`,
+    xhrFields: {
+      withCredentials: false
+    },
+    crossDomain: true,
     headers: {
-      api_key: 'UW6UfEEO71CGk2AZgexrHeGYiTRAy1GK',
-      q: search,
-      limit: 6,
-      'Access-Control-Allow-Origin': '*',
-      'Content-Security-Policy-Report-Only': true
+      // 'Access-Control-Request-Headers': 'http://localhost:7165',
+      // 'Content-Security-Policy-Report-Only': true
     }
   })
 }
--- assets/scripts/gif/ui.js
+++ assets/scripts/gif/ui.js
@@ -4,7 +4,7 @@ const gifTemplate = require('../templates/gif.handlebars')
 
 const displayGifs = function () {
   const gifs = store.gifs
-  console.log(gifs)
+  console.log(store)
   const htmlGifs = gifTemplate({gifs: gifs})
   $('#display-gif').html('')
   $('#display-gif').append(htmlGifs)
